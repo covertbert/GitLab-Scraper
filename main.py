74535150ed82842ae6c07a25e2fc2be31563a7a9
bertie.blackman <bertie.blackman@havas.com>
2017-10-13 17:01:39 +0100

    Fixed dotenv import for realz this time

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from urllib2 import Request, urlopen, URLError
 import simplejson as json
 import os
+from urllib2 import Request, urlopen, URLError
+from os.path import join, dirname
 from dotenv import load_dotenv
+
+# Dotenv loading
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Global Constants
 API_PRIVATE_TOKEN = os.environ.get('GITLAB_PRIVATE_TOKEN')
-API_KEY_QUERY = '?private_token='
+API_KEY_QUERY = '?private_token=' + API_PRIVATE_TOKEN
 API_BASE_URL = 'https://gitlab.havaslynx.com/api/v4/'
 
-# Dotenv loading
-dotenv_path = '.env'
-load_dotenv(dotenv_path)
-
 all_projects_request = Request(API_BASE_URL + '/projects' + API_KEY_QUERY)
-array_of_project_ids = []
 
 
 def create_projects_array():
+    array_of_project_ids = []
     try:
         response = urlopen(all_projects_request)
         projects = json.loads(response.read())
@@ -24,10 +25,11 @@ def create_projects_array():
         for project in projects:
             array_of_project_ids.append(project.get('id'))
 
+        print array_of_project_ids
+        return array_of_project_ids
+
     except URLError, e:
         print 'No Projects found', e
 
 
 create_projects_array()
-
-print array_of_project_ids
