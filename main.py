61cdf83a3e192ccfc7973c00eefcda2305e6d8f5
Bertie Blackman <blackmanrgh@gmail.com>
2017-10-14 18:15:34 +0100

    Refactored entire script to make it more DRY - No API request related functionality is now repeated

--- main.py
+++ main.py
@@ -20,26 +20,27 @@ def api_request(api_segment, other_queries):
     else:
         request = api_base_url + api_segment + api_key
 
-    return Request(request)
+    try:
+        response = urlopen(Request(request))
+        response_formatted = json.loads(response.read())
+
+        return response_formatted
+
+    except URLError, e:
+        print e
 
 
 def create_projects_array():
     all_projects = api_request('/projects', '&per_page=1000')
     array_of_project_ids = []
-    try:
-        response = urlopen(all_projects)
-        projects = json.loads(response.read())
 
-        for project in projects:
-            # Only push to array if London project
-            if project.get('namespace').get('name') == 'London':
-                array_of_project_ids.append(project.get('id'))
+    for project in all_projects:
+        # Only push to array if London project
+        if project.get('namespace').get('name') == 'London':
+            array_of_project_ids.append(project.get('id'))
 
-        print 'There are %s projects in the London namespace' % len(array_of_project_ids)
-        return array_of_project_ids
-
-    except URLError, e:
-        print 'No Projects found', e
+    print 'There are %s projects in the London namespace' % len(array_of_project_ids)
+    return array_of_project_ids
 
 
 def get_total_commits():
@@ -49,15 +50,8 @@ def get_total_commits():
     for project_id in project_ids:
         project_commits = api_request('/projects/' + str(project_id) + '/repository/commits', False)
 
-        try:
-            response = urlopen(project_commits)
-            commits = json.loads(response.read())
-
-            for commit in commits:
-                commits_array.append(commit)
-
-        except URLError, e:
-            print 'No Commits found', e
+        for commit in project_commits:
+            commits_array.append(commit)
 
     print 'Each project has an average of %s commits on the master branch' % (len(commits_array) / len(project_ids))
     print 'There is a total of %s commits on all master branches across the London namespace' % len(commits_array)
